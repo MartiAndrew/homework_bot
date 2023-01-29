80850890d3b73dd5aa5d50e88e0b504cbe36dc43
Andrew_Mishkov <andrew.mishkov@gmail.com>
2023-01-30 00:32:57 +0300

    final_refactoring

--- exceptions.py
+++ exceptions.py
@@ -2,3 +2,12 @@ class TelegramError(Exception):
     """Ошибка отправки телеграм-бота"""
     pass
 
+
+class InvalidResponseCode(Exception):
+    """Не верный код ответа."""
+    pass
+
+
+class EmptyResponseFromAPI(Exception):
+    """Пустой ответ от API"""
+    pass
--- homework.py
+++ homework.py
@@ -6,6 +6,7 @@ import sys
 import os
 from dotenv import load_dotenv
 import exceptions
+from http import HTTPStatus
 
 load_dotenv()
 
@@ -40,12 +41,18 @@ def send_message(bot, message):
         raise exceptions.TelegramError(
             f'Не удалось отправить сообщение {error}'
         )
+    else:
+        logging.debug(f'Сообщение отправлено {message}')
 
 
 def get_api_answer(timestamp):
     """Запрос к эндпоинту API Yandex Practicum"""
     try:
-        homeworks = requests.get(ENDPOINT, headers=HEADERS, params={'from_date': timestamp})
+        homeworks = requests.get(
+            ENDPOINT, headers=HEADERS, params={'from_date': timestamp}
+        )
+        if homeworks.status_code != HTTPStatus.OK:
+            raise exceptions.InvalidResponseCode('Не удалось получить ответ API')
         homeworks = homeworks.json()
         return homeworks
     except Exception as error:
@@ -55,11 +62,10 @@ def get_api_answer(timestamp):
 def check_response(response):
     """Проверяет ответ API на соответствие документации"""
     if isinstance(response, dict):
-        print('Ответ содержит словарь')
         if isinstance(response['homeworks'], list):
-            print('Ответ содержит список домашних работ')
             return True
-    print('Поменялась структура документации!')
+    if 'homeworks' not in response or 'current_date' not in response:
+        raise exceptions.EmptyResponseFromAPI('Пустой ответ от API')
     return False
 
 
@@ -67,6 +73,8 @@ def parse_status(homework):
     """Извлекает из информации статус домашней работы"""
     homework_name = homework.get('homework_name')
     status = homework.get('status')
+    if status not in HOMEWORK_VERDICTS:
+        raise ValueError(f'Неизвестный статус работы - {status}')
     verdict = HOMEWORK_VERDICTS[status]
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
@@ -85,18 +93,23 @@ def main():
                 message = parse_status(homework)
                 if message != preview_message:
                     send_message(bot, message)
+                else:
+                    logging.debug('Новые статусы работы отсутствуют')
                 preview_message = message
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
-            send_message(bot, message)
+            if message != preview_message:
+                send_message(bot, message)
+            preview_message = message
         finally:
             time.sleep(RETRY_PERIOD)
     else:
+        logging.critical('Отсутствие обязательных переменных окружения!')
 
 
 if __name__ == '__main__':
     logging.basicConfig(
-        level=logging.INFO,
+        level=logging.DEBUG,
         format=(
             '%(asctime)s, %(levelname)s, Путь - %(pathname)s, %(message)s'
         ),
